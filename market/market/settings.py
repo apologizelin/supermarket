05fe659cdc7a38b86a81b619889c4a471f6aed0d
linyi <lin863914273@163.com>
2018-11-18 16:48:55 +0800

    更新配置

--- market/market/settings.py
+++ market/market/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, "my.cnf"),  # 从文件中读取连接信息
+        },
     }
 }
 
